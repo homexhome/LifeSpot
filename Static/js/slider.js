2564323a2c24cdcd58f5002dcedc0300e01f2de6
homexhome <homexhome@proton.me>
2023-08-08 22:06:20 +0300

    minor fix

--- Static/js/slider.js
+++ Static/js/slider.js
@@ -5,11 +5,10 @@ const nextButton = document.querySelector(".next");
 let currentSlide = 1;
 
 function showSlides(n) {
-    let i;
-    let slides = document.querySelectorAll(".slide");;
+    let slides = document.querySelectorAll(".slide");
     if (n > slides.length) { currentSlide = 1 }
     if (n < 1) { currentSlide = slides.length }
-    for (i = 0; i < slides.length; i++) {
+    for (let i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
     slides[currentSlide - 1].style.display = "block";
